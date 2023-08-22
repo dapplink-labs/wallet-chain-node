95570bb3aa871571af7d34e872d5b3a52fcff3dd
shijiang.guo <shijiang@192.168.0.100>
2023-08-23 00:53:17 +0800

    fix bug

--- wallet/egld/types.go
+++ wallet/egld/types.go
@@ -1,12 +1,13 @@
 package egld
 
-type Account struct {
-	Address string
-}
-
-type TxByAddres struct {
-	Address  string
-	Page     uint64
-	PageSize uint64
-	Sort     string
-}
+//
+//type Account struct {
+//	Address string
+//}
+//
+//type TxByAddres struct {
+//	Address  string
+//	Page     uint64
+//	PageSize uint64
+//	Sort     string
+//}
